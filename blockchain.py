6b1af91e444b02182c27d67c2c6b11738b6ec113
rishab <rkjain02@ad.unc.edu>
2021-05-20 14:54:53 -0400

    proof of work for blockchain, finding a valid hash

--- blockchain.py
+++ blockchain.py
@@ -29,11 +29,24 @@ class Blockchain(object):
 
         return self.last_block['index']+1
 
-
+    @staticmethod
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
+    @property
     def last_block(self):
         return self.chain[-1]
+    
+    def proof_of_work(self,last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof_of_work
+
+    &staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode
+        guess_hash = hashlib.sha256(guess).hexidigest()
+        return guess_hash[:4] == "0000"
 
