ec01cca93d2adedfa23f6e87868097122c155e3e
rishab <rkjain02@ad.unc.edu>
2021-05-21 12:21:53 -0400

    Mining Endpoint

--- blockchain.py
+++ blockchain.py
@@ -66,7 +66,23 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+    blockchain.new_transaction(sender='0', recipient = node_identifier, amount=1)
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': 'New Block Forged',
+        'index' : block['index']
+        'transactions' : block['transaction'],
+        'proof' : block['proof'],
+        'previous_hash' : block['previous_hash'],
+    }
+
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['GET'])
 def new_transaction():
